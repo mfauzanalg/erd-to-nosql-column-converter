238f13d1444b1cdd0d1e5533af4ae41904f109c4
mfauzanalg <fauzan.alghifari@tiket.com>
2021-12-08 09:12:02 +0700

    Create weak shapes

--- MyConverter/samples/script.js
+++ MyConverter/samples/script.js
@@ -35,11 +35,56 @@ function init() {
         maxSize: new go.Size(120, NaN),
         textAlign: 'center',
         editable: true,
+        overflow: go.TextBlock.OverflowEllipsis
       },
       new go.Binding('text').makeTwoWay(),
       new go.Binding("isUnderline", "underline"),
     )
   );
+  
+  go.Shape.defineFigureGenerator("DoubleRectangle", function(shape, w, h) {
+    var geo = new go.Geometry();
+    var fig = new go.PathFigure();  // clockwise
+    geo.add(fig);
+    fig.add(new go.PathSegment(go.PathSegment.Line, 90, 0));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 90, 50));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 0, 50));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 0, 0));
+
+    fig.add(new go.PathSegment(go.PathSegment.Move, 4, 4));
+
+    fig.add(new go.PathSegment(go.PathSegment.Line, 86, 4));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 86, 46));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 4, 46));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 4, 4));
+
+    geo.spot1 = new go.Spot(0.156, 0.156);
+    geo.spot2 = new go.Spot(0.844, 0.844);
+    geo.defaultStretch = go.GraphObject.Uniform;
+    return geo;
+  });
+  
+  go.Shape.defineFigureGenerator("DoubleDiamond", function(shape, w, h) {
+    var geo = new go.Geometry();
+    var fig = new go.PathFigure(0, 25);  // clockwise
+    geo.add(fig);
+    fig.add(new go.PathSegment(go.PathSegment.Line, 60, 0));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 120, 25));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 60, 50));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 0, 25));
+
+    fig.add(new go.PathSegment(go.PathSegment.Move, 5, 25));
+
+    fig.add(new go.PathSegment(go.PathSegment.Line, 60, 3));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 115, 25));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 60, 47));
+    fig.add(new go.PathSegment(go.PathSegment.Line, 5, 25));
+
+    geo.spot1 = new go.Spot(0.156, 0.156);
+    geo.spot2 = new go.Spot(0.844, 0.844);
+    geo.defaultStretch = go.GraphObject.Uniform;
+    return geo;
+  });
 
   myDiagram.linkTemplate =
     $(go.Link,
@@ -48,8 +93,6 @@ function init() {
         new go.Binding("toArrow", "toArrow"),
       ),
     );
-  
-    
 
   // initialize Palette
   myPalette = $(go.Palette, 'myPaletteDiv', {
@@ -60,13 +103,15 @@ function init() {
 
   // now add the initial contents of the Palette
   myPalette.model.nodeDataArray = [
+    {text: '', figure: 'Ellipse', color:'white', height: 70},
     {text: 'Derived\nAttribute', color: 'black', figure: 'Ellipse', strokeDashArray: [2, 2]},
     {text: 'Identifier\nAttribute', color: 'black', figure: 'Ellipse', underline: true},
     {text: 'Regular\nAttribute', color: 'black', figure: 'Ellipse', },
-    {text: 'Relation', color: 'black', figure: 'Diamond'},
+    {text: 'Relation', color: 'black', figure: 'Diamond', width: 110, height: 50},
+    {text: 'Weak\nRelation', color: 'black', figure: 'DoubleDiamond', width: 120, height: 50},
     {text: 'Entity', color: 'black', figure: 'Rectangle', width: 90, height: 50},
+    {text: 'Weak\nEntity', color: 'black', figure: 'DoubleRectangle', width: 90, height: 50},
     {text: 'Is A', color: 'black', figure: 'TriangleDown'},
-    {text: 'Custom', color: 'black', figure: 'Custom'},
   ];
 
   var inspector = new Inspector('myInspectorDiv', myDiagram, {
