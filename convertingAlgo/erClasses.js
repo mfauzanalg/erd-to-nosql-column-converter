5b9fe032b526a669184ea7d08dd2ffacab3625eb
mfauzanalg <mfauzanalg@gmail.com>
2022-05-15 00:00:49 +0700

    ER and children migration to class based

--- convertingAlgo/erClasses.js
+++ convertingAlgo/erClasses.js
@@ -67,7 +67,7 @@ class EntityRelation {
   
       columnFamily.id = entityRelation.id
       columnFamily.label = entityRelation.label
-      const attributes = [...defineKey(entityRelation, columnFamilySet, columnFamily)];
+      const attributes = [...entityRelation.defineKey(entityRelation, columnFamilySet, columnFamily)];
       columnFamily.attributes = mergeArray(attributes, columnFamily.attributes || [])
       
       if (!columnFamily.attributes) {
@@ -92,7 +92,8 @@ class EntityRelation {
           const relationDetailArray = findRelationArray(entityRelation)
           relationDetailArray.forEach((relationDetail) => {
             relationDetail.relation.binaryType = relationDetail.type
-            columnFamilySet = mergeLogicalCF(columnFamilySet, convertRelationship(relationDetail, columnFamily, columnFamilySet))
+            console.log(relationDetail.relation);
+            columnFamilySet = mergeLogicalCF(columnFamilySet, relationDetail.relation.convertRelationship(relationDetail, columnFamily, columnFamilySet))
           })
         }
       }
@@ -100,6 +101,76 @@ class EntityRelation {
   
     if (returnNewCF) return columnFamily
     return columnFamilySet
+  }
+
+  defineKey (entityRelation, logicalCF, columnFamily) {
+    let key = [];
+    let ERKeys = getArrayKey(entityRelation.attributes)
+  
+    if (ERKeys.length > 0) {
+      key = duplicateArray(ERKeys)
+    }
+    if (['Entity', 'AssociativeEntity', 'WeakEntity'].includes(entityRelation.type)) {
+      key = [...ERKeys, ...findParentKey(entityRelation, logicalCF, columnFamily)]
+    }
+    else { // if thre type is Relationship
+      key = [...ERKeys, ...findRelationshipKey(entityRelation, logicalCF, columnFamily)]
+    }
+    if (key.length < 1 && !columnFamily.parentColumnFam && !(entityRelation.binaryType == "BinaryManyToMany")) {
+      key = [`id_${entityRelation.label}`]
+    }
+    return arrayKeysToAttribute(key)
+  }
+
+  convertRelationship (relationDetail, columnFamily, logicalCF) {
+    let newLogicalCF = [];
+    // Case1
+    if (['BinaryManyToOne', 'BinaryManyToMany', 'BinaryOneToOne', 'ReflexiveRelationship'].includes(relationDetail.type)) {
+      const columFamilyFromRelation = relationDetail.relation.createFamily(relationDetail.relation, logicalCF, true)
+  
+      if (relationDetail.relation?.attributes?.length > 0 && relationDetail.type === 'BinaryOneToOne') {
+        newLogicalCF.push(columFamilyFromRelation)
+      }
+  
+      if (relationDetail.type != 'BinaryOneToOne' && (!isSameKey(columnFamily, columFamilyFromRelation) || relationDetail.relation.type == 'ReflexiveRelationship')) {
+        newLogicalCF = [...newLogicalCF, ...relationDetail.relation.createArtificialRelation(columFamilyFromRelation, columnFamily, relationDetail, logicalCF)]
+      }
+    }
+    // Case 2 
+    else if (['AssociativeEntity'].includes(relationDetail.type)) {
+      const columFamilyFromAssociative = relationDetail.relation.createFamily(relationDetail.relation, logicalCF)
+      newLogicalCF = mergeLogicalCF(newLogicalCF, columFamilyFromAssociative)
+  
+      let type = 'BinaryManyToOne'
+      if (relationDetail.connector.cardinality === 'One') {
+        type = 'BinaryOneToOne'
+      }
+  
+      const tempRelation = new Relationship(
+        relationDetail.relation.ERModel,
+        relationDetail.relation.label,
+        'Relationship'
+      )
+      tempRelation.isTemporaryRelation = true
+      tempRelation.id = relationDetail.relation.id
+  
+      const temporaryRelationDetail = {
+        type: type,
+        relation: tempRelation
+      }
+  
+      const result = tempRelation.convertRelationship(temporaryRelationDetail, columnFamily, newLogicalCF)
+      newLogicalCF = mergeLogicalCF(result, newLogicalCF)
+  
+    }
+    // Case 3
+    else if (['SpecialConnector'].includes(relationDetail.type)) {
+      if (!relationDetail.relation?.isTotal) {
+        const parentColumnFamily = logicalCF.find(o => o.id === relationDetail.relation.superID);
+        newLogicalCF = [...newLogicalCF, ...relationDetail.relation.createArtificialRelation(columnFamily, parentColumnFamily, relationDetail, logicalCF)]
+      }
+    }
+    return newLogicalCF
   }
 
 }
@@ -113,6 +184,60 @@ class Entity extends EntityRelation {
 class Relationship extends EntityRelation {
   constructor(ERModel, label, type, connectors, attributes) {
     super(ERModel, label, type, connectors, attributes)
+  }
+
+  createArtificialRelation (columnFamily1, columnFamily2, relationDetail, logicalCF) {
+    let newLogicalCF = []
+    let newColumnFamily = undefined;
+  
+    if (columnFamily1.isFromRelationship) {
+      newColumnFamily = duplicateArray(columnFamily1)
+      
+    }
+    else {
+      const preexistentColumnFamily = logicalCF.find(o => o.id === columnFamily1.id);
+      if (preexistentColumnFamily.isAssociativeEntity) newColumnFamily = preexistentColumnFamily
+    }
+  
+    if (!newColumnFamily) {
+      const temporaryEntity = new Entity(
+        'ERModel', 
+        `${columnFamily1.label}_${columnFamily2.label}`,
+        'Entity'
+      )
+  
+      temporaryEntity.id = `temporary${columnFamily1.id}`
+      temporaryEntity.isTemporaryEntity = true
+      temporaryEntity.parentColumnFam = columnFamily1
+  
+    
+      newColumnFamily = temporaryEntity.createFamily(temporaryEntity, logicalCF, true)
+    }
+    
+    let auxAttribute = {};
+    auxAttribute.label = columnFamily2.label
+    auxAttribute.type = "Auxiliary";
+    auxAttribute.artificialID = artificialID;
+    newColumnFamily.attributes.push(auxAttribute)
+    
+    // Skip many to many, just like the original alog
+    let interAttribute = {};
+    if (relationDetail.type !== 'BinaryManyToMany') {
+      interAttribute.label = columnFamily1.label
+      interAttribute.artificialID = artificialID;
+      interAttribute.type = "Intermediary";
+      columnFamily2.attributes.push(interAttribute)
+    }
+    
+    artificialID += 1
+  
+    // handle many to many artificial relation
+    if (relationDetail.type === 'BinaryManyToMany') {
+      newColumnFamily.parentColumnFam = relationDetail.entityAcross.id
+    }
+  
+    newLogicalCF = mergeLogicalCF(newLogicalCF, [newColumnFamily, columnFamily2])
+    return newLogicalCF
   }
 }
 
