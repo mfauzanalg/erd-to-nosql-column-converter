e5b027c1059998b5d30d3f01197c3ab67771111d
mfauzanalg <mfauzanalg@gmail.com>
2022-06-17 20:29:54 +0700

    Fix attribute

--- components/er/erPalette.js
+++ components/er/erPalette.js
@@ -17,9 +17,9 @@ function initERPalette() {
     {text: 'Relation', color: 'black', figure: 'Diamond', width: 110, height: 50, fromLinkableDuplicates: true, fromMaxLinks:2},
     {text: 'Weak\nRelation', color: 'black', figure: 'DoubleDiamond', width: 120, height: 50},
     {text: 'Speciali\nzation', color: 'black', figure: 'TriangleDown', toLinkable: false, width: 120},
-    {text: 'Multivalued\nAttribute', color: 'black', figure: 'Ring', width: 100, height: 60},
-    {text: 'Derived\nAttribute', color: 'black', figure: 'Ellipse', strokeDashArray: [2, 2]},
-    {text: 'Identifier\nAttribute', color: 'black', figure: 'Ellipse', underline: true},
-    {text: 'Regular\nAttribute', color: 'black', figure: 'Ellipse'},
+    {text: 'Multivalued\nAttribute', color: 'black', figure: 'Ring', width: 100, height: 60, fromMaxLinks:1},
+    {text: 'Derived\nAttribute', color: 'black', figure: 'Ellipse', strokeDashArray: [2, 2], fromMaxLinks:1},
+    {text: 'Identifier\nAttribute', color: 'black', figure: 'Ellipse', underline: true, fromMaxLinks:1},
+    {text: 'Regular\nAttribute', color: 'black', figure: 'Ellipse', fromMaxLinks:1},
   ];
 }
